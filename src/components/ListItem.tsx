cf7e4ccaef3978ebc1d0f1e3d0adff6073dd7a8c
xinhuawu0501 <b06106035@ntu.edu.tw>
2023-08-09 09:11:28 +0800

    refactor: seperate bookmarked tab & all tab

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -11,67 +11,113 @@ import { DragEventHandler } from "../hooks/useDragDrop";
 import { useContext } from "react";
 import { TabCtx } from "../context/TabContextProvider";
 
-export const ListItem = ({
-  item,
-  category,
-  query,
-  handleDragStart,
-  handleDrop,
-}: {
+interface TabItemProps {
   item: ITabItem;
-  category: Catogories;
+  category?: Catogories;
   query: string;
   handleDragStart?: DragEventHandler["handleDragStart"];
   handleDrop?: DragEventHandler["handleDrop"];
-}) => {
+}
+
+const renderHighlightedTitle = (item: ITabItem, query: string) => {
+  const { searchedIndexes, info } = item;
+  const { title } = info;
+
+  const queryLength = query.length;
+  if (!searchedIndexes || searchedIndexes.length == 0 || queryLength === 0)
+    return title;
+
+  //split title string into chunks
+  const subStrs = [];
+  let p1 = 0;
+  for (let i = 0; i < searchedIndexes.length; i++) {
+    subStrs.push(title?.slice(p1, searchedIndexes[i]));
+
+    const highLightedStr = title?.slice(
+      searchedIndexes[i],
+      searchedIndexes[i] + queryLength
+    );
+
+    subStrs.push(highLightedStr);
+    p1 = searchedIndexes[i] + queryLength;
+    if (i == searchedIndexes.length - 1) subStrs.push(title?.slice(p1));
+  }
+  return (
+    <div>
+      {subStrs.map((str, i) =>
+        i % 2 == 0 ? <span key={i}>{str}</span> : <mark key={i}>{str}</mark>
+      )}
+    </div>
+  );
+};
+
+const BookmarkedTab = ({ item, query }: TabItemProps) => {
   const { title, url, favIconUrl, windowId, id } = item.info;
   const { ALL } = useContext(TabCtx);
 
-  const isDraggable = category == "ALL";
   const tabIsClosed = ALL.find((t) => t.info.id === id) == null;
-
-  const renderHighlightedTitle = () => {
-    const { searchedIndexes } = item;
-    const queryLength = query.length;
-    if (!searchedIndexes || searchedIndexes.length == 0 || queryLength === 0)
-      return title;
-
-    //split title string into chunks
-    const subStrs = [];
-    let p1 = 0;
-    for (let i = 0; i < searchedIndexes.length; i++) {
-      subStrs.push(title?.slice(p1, searchedIndexes[i]));
-
-      const highLightedStr = title?.slice(
-        searchedIndexes[i],
-        searchedIndexes[i] + queryLength
-      );
-
-      subStrs.push(highLightedStr);
-      p1 = searchedIndexes[i] + queryLength;
-      if (i == searchedIndexes.length - 1) subStrs.push(title?.slice(p1));
-    }
-    return (
-      <div>
-        {subStrs.map((str, i) =>
-          i % 2 == 0 ? <span key={i}>{str}</span> : <mark key={i}>{str}</mark>
-        )}
-      </div>
-    );
-  };
 
   return (
     <li
       id={String(windowId)}
       className={`${classes["tab-item"]} ${tabIsClosed && classes["close"]}`}
-      draggable={isDraggable}
+    >
+      {favIconUrl ? (
+        <img src={favIconUrl} alt={`${title}_img`} />
+      ) : (
+        <div className={classes["empty-img"]} />
+      )}
+
+      <div
+        className={classes["title"]}
+        onClick={() => {
+          item.handleNavigateTo(url!);
+        }}
+      >
+        {renderHighlightedTitle(item, query)}
+      </div>
+
+      {tabIsClosed ? (
+        <a
+          className={classes["icon-container"]}
+          href={url}
+          target="_blank"
+          rel="noopenner noreferrer"
+        >
+          <OpenLinkIcon />
+        </a>
+      ) : (
+        <button
+          className={classes["icon-container"]}
+          onClick={() => item.handleClose()}
+        >
+          <CloseIcon />
+        </button>
+      )}
+
+      <button
+        className={classes["icon-container"]}
+        onClick={() => item.handleToggleBookmark()}
+      >
+        {item.isBookmarked ? <BookmarkedIcon /> : <UnBookmarkedIcon />}
+      </button>
+    </li>
+  );
+};
+
+const Tab = ({ item, query, handleDragStart, handleDrop }: TabItemProps) => {
+  const { title, url, favIconUrl, windowId } = item.info;
+
+  return (
+    <li
+      id={String(windowId)}
+      className={`${classes["tab-item"]}`}
+      draggable={true}
       onDragStart={(e) => {
-        if (!isDraggable) return;
         handleDragStart!(e, item);
       }}
       onDragOver={(e) => e.preventDefault()}
       onDrop={(e) => {
-        if (!isDraggable) return;
         handleDrop!(e);
       }}
     >
@@ -85,34 +131,18 @@ export const ListItem = ({
       <div
         className={classes["title"]}
         onClick={() => {
-          //@ts-expect-error
-          item.handleNavigateTo(url);
+          item.handleNavigateTo(url!);
         }}
       >
-        {renderHighlightedTitle()}
+        {renderHighlightedTitle(item, query)}
       </div>
 
-      {!tabIsClosed && (
-        <button
-          className={classes["icon-container"]}
-          onClick={() => item.handleClose()}
-        >
-          <CloseIcon />
-        </button>
-      )}
-
-      {tabIsClosed ? (
-        <a
-          className={classes["icon-container"]}
-          href={url}
-          target="_blank"
-          rel="noopenner noreferrer"
-        >
-          <OpenLinkIcon />
-        </a>
-      ) : (
-        <></>
-      )}
+      <button
+        className={classes["icon-container"]}
+        onClick={() => item.handleClose()}
+      >
+        <CloseIcon />
+      </button>
 
       <button
         className={classes["icon-container"]}
@@ -121,5 +151,24 @@ export const ListItem = ({
         {item.isBookmarked ? <BookmarkedIcon /> : <UnBookmarkedIcon />}
       </button>
     </li>
+  );
+};
+
+export const ListItem = ({
+  item,
+  category,
+  query,
+  handleDragStart,
+  handleDrop,
+}: TabItemProps) => {
+  return category === "ALL" ? (
+    <Tab
+      item={item}
+      query={query}
+      handleDragStart={handleDragStart}
+      handleDrop={handleDrop}
+    />
+  ) : (
+    <BookmarkedTab item={item} query={query} />
   );
 };
