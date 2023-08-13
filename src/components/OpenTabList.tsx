c4f336309079046b53a067f6cc472ba02e1fd7b4
xinhuawu0501 <b06106035@ntu.edu.tw>
2023-08-13 12:03:58 +0800

    feat: make `OpenTab` draggable

--- src/components/OpenTabList.tsx
+++ src/components/OpenTabList.tsx
@@ -1,9 +1,15 @@
-import { useContext, useMemo } from "react";
+import { useContext, useEffect, useMemo } from "react";
 import { TabCtx } from "../context/TabContextProvider";
 import { ITabItem } from "../lib/type/Tab";
 import classes from "../styles/Tab.module.css";
 import { ListItem } from "./ListItem";
 import { SearchCtx } from "../context/SearchContextProvider";
+import {
+  DragDropContext,
+  Draggable,
+  Droppable,
+  resetServerContext,
+} from "react-beautiful-dnd";
 
 const handleGroupTabsByWindow = (tabs: ITabItem[]) => {
   const groups: { [id: number]: ITabItem[] } = {};
@@ -20,7 +26,6 @@ const handleGroupTabsByWindow = (tabs: ITabItem[]) => {
 export const OpenTabList = () => {
   const { ALL, window } = useContext(TabCtx);
   const { data, query } = useContext(SearchCtx);
-
   const { searchedAllTabs } = data;
 
   const allTabArrSortedByWindow = useMemo(() => {
@@ -40,22 +45,50 @@ export const OpenTabList = () => {
     return arr;
   }, [ALL, searchedAllTabs, window]);
 
-  const renderAllTabs = (tabs: ITabItem[], windowId: string) => {
-    return (
-      <ul className={classes["tab-group"]} id={windowId}>
-        {tabs.map((t) => (
-          <ListItem key={t.info.id} item={t} category="ALL" query={query} />
-        ))}
-      </ul>
-    );
-  };
-
-  console.log(allTabArrSortedByWindow);
+  useEffect(() => {
+    resetServerContext();
+  }, []);
 
   return (
     <div id="all-tabs">
       <label>{`ALL (${searchedAllTabs.length})`}</label>
-      {allTabArrSortedByWindow.map(([key, value]) => renderAllTabs(value, key))}
+      <DragDropContext onDragEnd={() => {}}>
+        {allTabArrSortedByWindow.map(([windowId, tabs]) => {
+          return (
+            <Droppable droppableId={windowId} key={windowId}>
+              {(provided) => {
+                const { droppableProps, innerRef } = provided;
+                return (
+                  <ul
+                    className={classes["tab-group"]}
+                    id={windowId}
+                    {...droppableProps}
+                    ref={innerRef}
+                  >
+                    {tabs.map((t, i) => (
+                      <Draggable draggableId={t.info.id! + ""} index={i}>
+                        {(provided) => {
+                          const { dragHandleProps, draggableProps } = provided;
+                          return (
+                            <ListItem
+                              {...dragHandleProps}
+                              {...draggableProps}
+                              key={t.info.id}
+                              item={t}
+                              category="ALL"
+                              query={query}
+                            />
+                          );
+                        }}
+                      </Draggable>
+                    ))}
+                  </ul>
+                );
+              }}
+            </Droppable>
+          );
+        })}
+      </DragDropContext>
     </div>
   );
 };
